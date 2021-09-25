72c3224e4c1da7b21a4187cf2c96de6fa666a872
Aleksei <avoroncovs@gmail.com>
2021-09-25 18:06:57 +0300

    added first 'genDiff' test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,13 @@
+import genDiff from '../src/gendiff.js';
+
 test('1', () => {
-  expect(1).toEqual(1);
+  const result = genDiff('__tests__/resources/1.json', '__tests__/resources/2.json');
+  expect(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  + timeout: 20
+  - timeout: 50
+  + verbose: true
+}`).toEqual(result);
 });
