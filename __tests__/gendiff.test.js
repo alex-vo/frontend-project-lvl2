abe0d6fcc12e4497ea4e2f4a5c38cb2cbeab7faa
Aleksei <avoroncovs@gmail.com>
2021-09-26 08:30:05 +0300

    restored 'should generate diff for 2 json files' test case

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,13 @@
 import genDiff from '../src/gendiff.js';
 
 test('should generate diff for 2 json files', () => {
-  try {
-    genDiff('non-existing-file.json', '__tests__/__fixtures__/2.json');
-  } catch (e) {
-    console.log(e);
-  }
+  const result = genDiff('__tests__/__fixtures__/1.json', '__tests__/__fixtures__/2.json');
+  expect(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  + timeout: 20
+  - timeout: 50
+  + verbose: true
+}`).toEqual(result);
 });
