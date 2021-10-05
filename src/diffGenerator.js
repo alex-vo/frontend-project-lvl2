4a163c4542ed1992052a67a36cf2ac3f99e2cee1
Aleksei <avoroncovs@gmail.com>
2021-10-05 12:28:00 +0300

    disabled 'object-curly-newline' in .eslintrc.yml, minor polish of diffGenerator.js

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -9,17 +9,17 @@ const generateDiff = (data1, data2) =>
       const data2Value = data2[key];
       if (!_.has(data1, key)) {
         return { key, type: 'added', newValue: data2Value };
-      } else if (!_.has(data2, key)) {
-        return { key, type: 'removed', oldValue: data1Value };
-      } else {
-        if (isNonArrayObject(data1Value) && isNonArrayObject(data2Value)) {
-          return { key, type: 'nested', newValue: generateDiff(data1Value, data2Value) };
-        } else if (!_.isEqual(data1Value, data2Value)) {
-          return { key, type: 'changed', oldValue: data1Value, newValue: data2Value };
-        } else {
-          return { key, type: 'none', newValue: data1Value };
-        }
       }
-    })
+      if (!_.has(data2, key)) {
+        return { key, type: 'removed', oldValue: data1Value };
+      }
+      if (isNonArrayObject(data1Value) && isNonArrayObject(data2Value)) {
+        return { key, type: 'nested', newValue: generateDiff(data1Value, data2Value) };
+      }
+      if (!_.isEqual(data1Value, data2Value)) {
+        return { key, type: 'changed', oldValue: data1Value, newValue: data2Value };
+      }
+      return { key, type: 'none', newValue: data1Value };
+    });
 
 export default generateDiff;
